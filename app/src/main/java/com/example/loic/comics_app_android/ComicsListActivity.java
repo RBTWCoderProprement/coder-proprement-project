3c62e6d0d2600b74fcefa62b37a32e24316c5cf9
Loic <Loic-0607@hotmail.fr>
2018-11-23 20:59:30 +0100

    [FEAT] Init project

---
+++ app/src/main/java/com/example/loic/comics_app_android/ComicsListActivity.java
@@ -0,0 +1,13 @@
+package com.example.loic.comics_app_android;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class ComicsListActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_comics_list);
+    }
+}
