952c876b84a74ef2fd862c3adc220d69dbf1931a
Loic <Loic-0607@hotmail.fr>
2018-12-02 21:42:34 +0100

    [FEAT] add get by id in manager with gson and single

--- app/src/main/java/com/example/loic/comics_app_android/data/manager/ComicsManagerImpl.java
+++ app/src/main/java/com/example/loic/comics_app_android/data/manager/ComicsManagerImpl.java
@@ -1,4 +1,68 @@
 package com.example.loic.comics_app_android.data.manager;
 
+
+import android.content.res.Resources;
+import android.util.Log;
+
+import com.example.loic.comics_app_android.R;
+import com.example.loic.comics_app_android.data.model.Comic;
+import com.example.loic.comics_app_android.data.model.ResultsItem;
+import com.google.gson.Gson;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.InputStreamReader;
+
+import io.reactivex.Single;
+
 public class ComicsManagerImpl {
+
+    private int jsonSuccess = R.raw.comic_succes;
+    private Resources resources;
+
+    public ComicsManagerImpl(Resources assetManager) {
+        this.resources = assetManager;
+    }
+
+    public Single<ResultsItem> getComicById(int id) {
+        return Single.create(subsciber -> {
+            Gson gson = new Gson();
+            Comic comics = gson.fromJson(FileToString(), Comic.class);
+            if(comics.getCode() != 500) {
+                subsciber.onError(new Throwable("Error Code"));
+                return;
+            }
+            for(ResultsItem result: comics.getResults()) {
+                if (result.getId() == id) {
+                    subsciber.onSuccess(result);
+                }
+            }
+            subsciber.onError(new Throwable("Error Not found"));
+        });
+    }
+
+    private String FileToString(){
+        InputStream inputStream = resources.openRawResource(jsonSuccess);
+        try {
+            // open a reader on the inputStream
+            BufferedReader reader = new BufferedReader(new InputStreamReader(inputStream));
+
+            // String used to store the lines
+            String str;
+            StringBuilder buf = new StringBuilder();
+
+            // Read the file
+            while ((str = reader.readLine()) != null) {
+                buf.append(str).append("\r\n");
+            }
+            // Close streams
+            reader.close();
+            inputStream.close();
+            return buf.toString();
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+        return "";
+    }
 }
--- app/src/main/java/com/example/loic/comics_app_android/presentation/ui/activity/ComicsListActivity.java
+++ app/src/main/java/com/example/loic/comics_app_android/presentation/ui/activity/ComicsListActivity.java
@@ -1,15 +1,40 @@
 package com.example.loic.comics_app_android.presentation.ui.activity;
 
-import android.support.v7.app.AppCompatActivity;
+import android.annotation.SuppressLint;
 import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.util.Log;
 
 import com.example.loic.comics_app_android.R;
+import com.example.loic.comics_app_android.data.manager.ComicsManagerImpl;
+import com.example.loic.comics_app_android.data.model.ResultsItem;
+
+import io.reactivex.Scheduler;
+import io.reactivex.observers.DisposableSingleObserver;
+import io.reactivex.schedulers.Schedulers;
 
 public class ComicsListActivity extends AppCompatActivity {
+
+    private ComicsManagerImpl manage;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_comics_list);
+        manage = new ComicsManagerImpl(getResources());
+        manage.getComicById(64966).subscribeOn(Schedulers.io()).subscribeWith(new DisposableSingleObserver<ResultsItem>() {
+            @Override
+            public void onSuccess(ResultsItem resultsItem) {
+                Log.d("mlk",resultsItem.toString());
+            }
+
+            @Override
+            public void onError(Throwable e) {
+                Log.d("mlk", e.getMessage());
+                e.printStackTrace();
+            }
+        }).isDisposed();
+
     }
 }
