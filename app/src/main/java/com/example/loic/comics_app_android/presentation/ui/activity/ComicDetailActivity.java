ac1ce4065a4931a606da06ef393332b5cd12bd74
Morgane <morgane0798@hotmail.fr>
2018-12-30 23:41:43 +0100

    [TASK] Init creators list in comic detail, added menu and icon

--- app/src/main/java/com/example/loic/comics_app_android/presentation/ui/activity/ComicDetailActivity.java
+++ app/src/main/java/com/example/loic/comics_app_android/presentation/ui/activity/ComicDetailActivity.java
@@ -1,26 +1,37 @@
 package com.example.loic.comics_app_android.presentation.ui.activity;
 
 import android.support.v7.app.AppCompatActivity;
-import android.support.v7.widget.RecyclerView;
 import android.os.Bundle;
-import android.support.v7.widget.LinearLayoutManager;
+import android.support.v7.widget.RecyclerView;
+import android.util.Log;
+import android.view.Menu;
 import android.view.MenuItem;
+import android.widget.ImageView;
+import android.widget.ListView;
+import android.widget.TextView;
 
 import com.example.loic.comics_app_android.R;
 import com.example.loic.comics_app_android.data.model.CreatorsItem;
 import com.example.loic.comics_app_android.data.model.ResultsItem;
-import com.example.loic.comics_app_android.presentation.ui.adapter.RecyclerComicDetailAdapter;
+import com.example.loic.comics_app_android.presentation.ui.adapter.ComicDetailCreatorsListAdapter;
+import com.squareup.picasso.Picasso;
 
+import java.text.ParseException;
+import java.text.SimpleDateFormat;
 import java.util.ArrayList;
+import java.util.Date;
 import java.util.List;
 
 public class ComicDetailActivity extends AppCompatActivity {
-    private RecyclerView mRecyclerView;
-    private RecyclerView.Adapter mAdapter;
-    private RecyclerView.LayoutManager mLayoutManager;
-
-    private ResultsItem comic;
     private List<CreatorsItem> creators = new ArrayList<>();
+
+    public ImageView comicImage;
+    public TextView comicTitle;
+    public TextView comicSynopsis;
+    public TextView comicDate;
+    public TextView comicPagePriceDiamond;
+    public RecyclerView comicCreatorsList;
+    private RecyclerView.Adapter creatorsListAdapter;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -49,13 +60,6 @@ public class ComicDetailActivity extends AppCompatActivity {
             }).isDisposed();
          */
 
-        mRecyclerView = (RecyclerView) findViewById(R.id.comic_detail_rv);
-        mRecyclerView.setHasFixedSize(true);
-
-        mLayoutManager = new LinearLayoutManager(this);
-        mRecyclerView.setLayoutManager(mLayoutManager);
-
-
         // Test avec des données en dur
         ResultsItem comic = new ResultsItem();
         comic.setTitle("Despicable Deadpool (2017) #288");
@@ -83,16 +87,59 @@ public class ComicDetailActivity extends AppCompatActivity {
         comic.setImage("http://i.annihil.us/u/prod/marvel/i/mg/f/03/59e7b08528560.jpg");
         comic.setPageCount(32);
 
-        mAdapter = new RecyclerComicDetailAdapter(comic);
-        mRecyclerView.setAdapter(mAdapter);
+        comicImage = findViewById(R.id.comic_detail_image);
+        comicTitle = findViewById(R.id.comic_detail_title);
+        comicSynopsis = findViewById(R.id.comic_detail_synopsis);
+        comicDate = findViewById(R.id.comic_detail_date);
+        comicPagePriceDiamond = findViewById(R.id.comic_detail_price_page_diamond);
+        comicCreatorsList = findViewById(R.id.comic_detail_creators_list);
+
+//        mLayoutManager = new LinearLayoutManager(this);
+//        mRecyclerView.setLayoutManager(mLayoutManager);
+
+        Picasso.get().load(comic.getImage()).into(comicImage);
+        comicTitle.setText(comic.getTitle());
+        comicSynopsis.setText(comic.getDescription());
+        comicDate.setText(parseTimeStamp(comic.getDate()));
+        comicPagePriceDiamond.setText(comic.getPrice() + " $, "+ comic.getPageCount() + " pages - " + comic.getDiamondCode());
+
+        creatorsListAdapter = new ComicDetailCreatorsListAdapter(comic.getCreators());
+        comicCreatorsList.setAdapter(creatorsListAdapter);
+
+//        final ArrayAdapter<String> comicCreatorsAdapter = new ArrayAdapter<String>(ComicDetailActivity.this,
+//                android.R.layout.simple_list_item_1, prenoms);
+//        comicCreatorsList.setAdapter(comicCreatorsAdapter);
+//        holder.comicCreatorsList.setAdapter(comicCreatorsList);
     }
 
     public boolean onOptionsItemSelected(MenuItem item){
-        int itemId = item.getItemId();
-        if (itemId==android.R.id.home) {
-            finish();
-            return true;
+        switch (item.getItemId()){
+            case R.id.home:
+                finish();
+                return true;
+            case R.id.menu_partager:
+                //shareComic();
+                return true;
         }
         return super.onOptionsItemSelected(item);
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        getMenuInflater().inflate(R.menu.menu_comic_detail, menu);
+        return true;
+    }
+
+    public String parseTimeStamp(String dateTimeStamp) {
+        Date d;
+        SimpleDateFormat dateFormat = new SimpleDateFormat("yyyy-MM-dd'T'HH:mm:ssZ");
+        try {
+            d = dateFormat.parse(dateTimeStamp);
+            dateFormat.applyPattern("dd/MM/yyyy");
+        } catch (ParseException e) {
+            Log.e("ComicDateParseError", "Could not parse comic date");
+            return "";
+        }
+        return dateFormat.format(d);
     }
 }
