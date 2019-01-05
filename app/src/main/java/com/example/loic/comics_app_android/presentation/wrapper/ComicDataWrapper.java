8b3d06985fc654c390c2eec3e57a7ca0908e526f
Loic <Loic-0607@hotmail.fr>
2019-01-05 14:45:12 +0100

    [FEAT] add Share comic

--- app/src/main/java/com/example/loic/comics_app_android/presentation/wrapper/ComicDataWrapper.java
+++ app/src/main/java/com/example/loic/comics_app_android/presentation/wrapper/ComicDataWrapper.java
@@ -32,6 +32,10 @@ public class ComicDataWrapper {
         return comic.getPrice() + " $, " + comic.getPageCount() + " pages - " + comic.getDiamondCode();
     }
 
+    public String contentShare() {
+        return comic.getTitle() + "\n\n "+ comic.getDescription() + "\n\n" + parseTimeStamp() + " $\nPrix: " + comic.getPrice();
+    }
+
     public ResultsItem getComic() {
         return this.comic;
     }
